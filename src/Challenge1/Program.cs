671b1e2068998cfb28ddfc0b892672c10519ecfb
Håkon Nordli <hakon.nordli@xlent.no>
2023-11-17 20:23:03 +0100

    first commit

---
+++ src/Challenge1/Program.cs
@@ -0,0 +1,36 @@
+using Challenge1.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TheAppContext>(opt => opt.UseInMemoryDatabase(DbData.DbName));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+var theScope = app.Services.CreateScope();
+var context = theScope.ServiceProvider.GetRequiredService<TheAppContext>();
+DbData.AddMockData(context);
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
