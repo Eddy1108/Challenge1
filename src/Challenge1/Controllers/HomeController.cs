67783eb699b60ca9ffae63e94d30be2c0de5e755
Eddy1108 <edvardh@hotmail.no>
2024-03-23 15:26:52 +0100

    Update HomeController.cs

--- src/Challenge1/Controllers/HomeController.cs
+++ src/Challenge1/Controllers/HomeController.cs
@@ -1,22 +1,35 @@
+using System.Collections;
 using Challenge1.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Challenge1.Data;
 using Challenge1.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace Challenge1.Controllers
 {
     public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+		private readonly TheAppContext _theAppContext;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ILogger<HomeController> logger, TheAppContext appContext)
 		{
 			_logger = logger;
+			_theAppContext = appContext;
 		}
 
 		public async Task<ViewResult> Index()
 		{
-			var homeModel = new HomeModel() {};
+			//get the customers from the database
+			var customers = await _theAppContext.Customers.ToListAsync();
+
+			var homeModel = new HomeModel()
+			{
+				//pass the customers to the model
+				Customers = customers
+
+			};
 			return View(homeModel);
 		}
 
