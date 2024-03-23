8b258a7b9ad04b15ccb330420a25aa0ff0cde332
Eddy1108 <edvardh@hotmail.no>
2024-03-23 15:51:12 +0100

    Update DbData.cs

--- src/Challenge1/Data/DbData.cs
+++ src/Challenge1/Data/DbData.cs
@@ -10,10 +10,13 @@ namespace Challenge1.Data
 			Customer customer1 = new Customer() { Id = 1, FirstName = "Lyn", LastName ="Gordon" };
 			Customer customer2 = new Customer() { Id = 2, FirstName = "Donald", LastName = "Duck" };
 			Customer customer3 = new Customer() { Id = 3, FirstName = "Clark", LastName = "Kent" };
+			Customer customer4 = new Customer() { Id = 4, FirstName = "Nigel", LastName = "Sibbert" };
+
 
 			context.Add(customer1);
 			context.Add(customer2);
 			context.Add(customer3);
+			context.Add(customer4);
 			context.SaveChanges();
 		}
 	}
